643c09dbfb2929e1b00e9c4bc43339c46c11f571
DeiVis <zbitkisas@gmail.com>
2024-08-27 15:21:29 +0300

    up

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const body = document.querySelector('body');
 const hex = document.querySelector('.hex');
-const btn = document.querySelector('button');
+const btn = document.querySelector('button');``
 
 btn.addEventListener('click', () => {
     const randomColor = Math.random().toString(16).slice(2,8);
